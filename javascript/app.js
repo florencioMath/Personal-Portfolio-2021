bbde888880d1c5ce44ac86116a64643523784eb5
1matheusflorencio <68713424+1matheusflorencio@users.noreply.github.com>
2021-10-29 07:05:54 -0300

    Trabalhando no burguer

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+    const burger = document.querySelector(".container__burguer");
+    const nav = document.querySelector(".container__links");
+    const navLinks = document.querySelectorAll(".container__links li");
   
     burger.addEventListener("click", () => {
       nav.classList.toggle("nav-active");
