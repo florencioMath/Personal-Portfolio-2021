345ad01db6ec9cac0f91fcca677ab6d61e3739e0
1matheusflorencio <68713424+1matheusflorencio@users.noreply.github.com>
2021-10-30 14:56:57 -0300

    Atuallização no Burguer do Mobile, JS funcinando

--- javascript/app.js
+++ javascript/app.js
@@ -8,10 +8,10 @@ const navSlide = () => {
   
       navLinks.forEach((link, index) => {
         if (link.style.animation) {
-          link.style.animation = "";
+          link.style.animation = '';
         } else {
           link.style.animation =
-            "navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s";
+          `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
         }
       });
       burger.classList.toggle("toggle");
