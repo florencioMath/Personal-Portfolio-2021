9a55ac30c583118ca4f6e51deaef9bae281d7ddb
1matheusflorencio <68713424+1matheusflorencio@users.noreply.github.com>
2021-11-07 18:15:23 -0300

    Modificações na responsividade do burguer

--- javascript/app.js
+++ javascript/app.js
@@ -1,22 +1,35 @@
 const navSlide = () => {
-    const burger = document.querySelector(".container__burguer");
-    const nav = document.querySelector(".container__links");
-    const navLinks = document.querySelectorAll(".container__links li");
-  
-    burger.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
-  
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation =
+  const burger = document.querySelector(".container__burguer");
+  const nav = document.querySelector(".container__links");
+  const navLinks = document.querySelectorAll(".container__links li");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation =
           `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
-        }
-      });
-      burger.classList.toggle("toggle");
+      }
     });
-  };
+    burger.classList.toggle("toggle");
+  });
   
-  navSlide();
-  
+  nav.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation =
+          `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
